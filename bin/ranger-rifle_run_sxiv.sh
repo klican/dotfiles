b1f79f2805eb3e27cbf94e2391e17b67a70b1b16
Andrej Klič <andrej.klic@gmail.com>
2012-05-15 16:55:45 +0200

    Script ~/bin/ranger-rifle_run_sxiv.sh:
    Need to fix regex ! - seems to not open JPG/JPEG, only lovercase - jpg/jpeg

--- bin/ranger-rifle_run_sxiv.sh
+++ bin/ranger-rifle_run_sxiv.sh
@@ -2,7 +2,7 @@
 target="$(realpath -s "$1")"
 [ "$1" == '--' ] && shift
 function listfiles {
-  find "$(dirname "$target")" -maxdepth 1 -type f -regex '.*\(jpe?g\|png\|gif\)$' -print0 | sort -z
+  find "$(dirname "$target")" -maxdepth 1 -type f -regex '.*\(jpe?g\|JPE?G\|png\|gif\)$' -print0 | sort -z
 }
 count="$(listfiles | grep -m 1 -Zznx "$target" | cut -d: -f1)"
 
