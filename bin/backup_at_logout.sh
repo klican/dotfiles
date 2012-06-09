d7f9486b621c1d0beedbbee517e5e222b94b0ecb
Andrej Klič <andrej.klic@gmail.com>
2012-06-09 13:12:54 +0200

    Disabled rsync backup and restore on login/halt

--- bin/backup_at_logout.sh
+++ bin/backup_at_logout.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-/home/lelio/bin/pack_pentadactyl.sh
-/home/lelio/bin/pack_macromedia.sh
-/home/lelio/bin/pack_adobe.sh
-/home/lelio/bin/pack_google-googletalkplugin.sh
-#/home/lelio/bin/pack_gvfs.sh
-/home/lelio/bin/pack_gconf.sh
-/home/lelio/bin/pack_gconfd.sh
-#/home/lelio/bin/pack_purple.sh
-/home/lelio/bin/pack_Skype.sh
+#/home/lelio/bin/pack_pentadactyl.sh
+#/home/lelio/bin/pack_macromedia.sh
+#/home/lelio/bin/pack_adobe.sh
+#/home/lelio/bin/pack_google-googletalkplugin.sh
+##/home/lelio/bin/pack_gvfs.sh
+#/home/lelio/bin/pack_gconf.sh
+#/home/lelio/bin/pack_gconfd.sh
+##/home/lelio/bin/pack_purple.sh
+#/home/lelio/bin/pack_Skype.sh
