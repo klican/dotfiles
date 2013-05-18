19709d83ba6fc4915d94e093aa53a7aa70f27a18
Andrej Klič <andrej.klic@gmail.com>
2013-05-18 14:20:24 +0200

    awesomewm: update flash rules

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -542,7 +542,7 @@ awful.rules.rules = {
       properties = { floating = true }, callback = awful.titlebar.add  },
     { rule = { class = "Xmessage", instance = "xmessage" },
       properties = { floating = true }, callback = awful.titlebar.add  },
-    { rule = { instance = "firefox-bin" },
+    { rule = { instance = "plugin-container" },
       properties = { floating = false } },
     { rule = { instance = "stardict" },
       properties = { floating = false } },
